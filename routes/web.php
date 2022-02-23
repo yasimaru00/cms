95c2a2fea83a263453a1bf1154e76dfa47d60e08
yasimaru00 <yasimaru00@gmail.com>
2022-02-23 19:38:10 +0700

    Perubahan routes dengan menggunakan Page Controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,15 +13,21 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+// Praktikum 1 Routes
 
-Route::get('/', function () {
-    echo " Hi! Selamat Datang di Website Laravel";
-});
-Route::get('/about', function () {
-    echo " NIM : 2141723001 <br><br>";
-    echo " GALILEY SINGGANG M.Y <br><br>";
-    echo " TI 2G";
-});
-Route::get('/articles/{id}', function ($id) {
-    echo " Ini adalah halaman Artikel dengan ID : {$id}";
-});
+// Route::get('/', function () {
+//     echo " Hi! Selamat Datang di Website Laravel";
+// });
+// Route::get('/about', function () {
+//     echo " NIM : 2141723001 <br><br>";
+//     echo " GALILEY SINGGANG M.Y <br><br>";
+//     echo " TI 2G";
+// });
+// Route::get('/articles/{id}', function ($id) {
+//     echo " Ini adalah halaman Artikel dengan ID : {$id}";
+// });
+
+// Praktikum 2 Controller
+Route::get('/', [PageController::class, 'index']);
+Route::get('/about', [PageController::class, 'about']);
+Route::get('/articles/{id}', [PageController::class, 'articles']);
