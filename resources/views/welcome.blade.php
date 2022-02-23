50b7e8c99dfd56b1b69db01eaa44c685e98d464f
yasimaru00 <yasimaru00@gmail.com>
2022-02-23 19:24:13 +0700

    Perubahan nama

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">         
-            <div class="ml-4  text-lg text-gray-900 sm:text-left sm:ml-0">
+            <div class="ml-4  text-lg text-gray-900 sm:text-left, sm:ml-0">
                 GALILEY SINGGANG MANGKULUHUR YASIMARU<br><br>
                 NIM: 2141723001, Kelas: TI-2G
             </div>
