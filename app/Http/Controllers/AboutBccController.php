a3b493055f6caa1bb5ead114c331960db01dd922
yasimaru00 <yasimaru00@gmail.com>
2022-02-23 22:15:24 +0700

    Pembuatan AboutBcc Controller

---
+++ app/Http/Controllers/AboutBccController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutBccController extends Controller
+{
+    public function AboutBcc()
+    {
+        return " <p> We’re impartial and independent, and every day we create distinctive, world-class programmes and content which inform, educate and entertain millions of people in the UK and around the world. </p>";
+    }
+}
