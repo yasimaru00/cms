e0d89f1a62834e3d414c5fbb481aa48b535e97d6
yasimaru00 <yasimaru00@gmail.com>
2022-02-23 20:02:10 +0700

    Pembuatan  About Controller

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function index()
+    {
+        echo " NIM : 2141723001 <br><br>";
+        echo " GALILEY SINGGANG M.Y <br><br>";
+        echo " TI 2G";
+    }
+}
