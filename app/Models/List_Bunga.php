be21e215f7e861b71b98fc62bb355fbb8336590e
yasimaru00 <yasimaru00@gmail.com>
2022-03-27 01:14:48 +0700

    penambahan dan perbaikan model untuk job3

---
+++ app/Models/List_Bunga.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class List_Bunga extends Model
+{
+    use HasFactory;
+
+    protected $table = 'list_bunga';
+}
